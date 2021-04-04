ee8980f5f1ea388da9fc702b5e3523914d34c6f9
root <root@vm.sugodipesce.net>
2021-04-04 19:56:19 +0200

    add find_account_from_domain.py

---
+++ find_account_from_domain.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import os,sys,pwd
+
+if len(sys.argv) != 2:
+	print('\nUsage: ./find_account_from_domain.py DOMAIN\n')
+	exit('Exiting...')
+
+if (os.getuid() != 0):
+	print('Please be root or use sudo.\n')
+	exit()
+
+os.chdir('/etc/valiases')
+
+#if os.path.isfile(/etc/valiases/argv[1]:
+#print('Domain ' + str(sys.argv[1]) + ' belongs to cPanel account: ' + str(os.path.owner(sys.argv[1])))
+print('Domain ' + str(sys.argv[1]) + ' belongs to cPanel account: ' + str(pwd.getpwuid(os.stat(sys.argv[1]).st_uid).pw_name))
